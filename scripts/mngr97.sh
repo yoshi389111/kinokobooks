54554af33c1acf81865653d2723b597a01658e5b
sato yoshiyuki <yoshi.389111@gmail.com>
2019-03-21 21:02:14 +0900

    :sparkles: add: プロジェクト・マネジャーが知るべき97のこと

---
+++ scripts/mngr97.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $(dirname $0)
+cd ../src/mngr
+
+export LANG="ja_JP.UTF-8"
+
+pandoc -f markdown -t epub3-auto_identifiers \
+ --toc-depth=1 \
+ --epub-cover-image=./mngr.png \
+ --epub-stylesheet=../epub.css \
+ -o ../../epub/mngr97.epub \
+ mngr000.txt mngr*.md
+
